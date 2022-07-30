e2d2b9d526632d3eec1c2390062658893fc04796
yarinbnyamin <bnyamin@post.bgu.ac.il>
2022-07-30 16:22:30 +0300

    changes for server to work

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ var path = require("path");
 var logger = require("morgan");
 const session = require("client-sessions");
 const DButils = require("./routes/utils/DButils");
-var cors = require('cors')
+var cors = require("cors");
 
 var app = express();
-app.use(logger("dev")); //logger
+app.use(logger("dev")); // logger
 app.use(express.json()); // parse application/json
 app.use(
   session({
@@ -19,23 +19,21 @@ app.use(
     activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration,
     cookie: {
       httpOnly: false,
-    }
+    },
     //the session will be extended by activeDuration milliseconds
   })
 );
 app.use(express.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(express.static(path.join(__dirname, "public"))); //To serve static files such as images, CSS files, and JavaScript files
 //local:
-app.use(express.static(path.join(__dirname, "dist")));
+//app.use(express.static(path.join(__dirname, "dist")));
 //remote:
-// app.use(express.static(path.join(__dirname, '../assignment-3-3-basic/dist')));
-app.get("/",function(req,res)
-{ 
-  //remote: 
-  // res.sendFile(path.join(__dirname, '../assignment-3-3-basic/dist/index.html'));
+app.use(express.static(path.join(__dirname, "../assignment-3-3-basic/dist")));
+app.get("/", function (req, res) {
+  //remote:
+  res.sendFile(path.join(__dirname, "../assignment-3-3-basic/dist/index.html"));
   //local:
-  res.sendFile(__dirname+"/index.html");
-
+  //res.sendFile(__dirname + "/index.html");
 });
 
 // app.use(cors());
@@ -43,7 +41,7 @@ app.get("/",function(req,res)
 
 const corsConfig = {
   origin: true,
-  credentials: true
+  credentials: true,
 };
 
 app.use(cors(corsConfig));
@@ -54,7 +52,6 @@ var port = process.env.PORT || "80"; //local=3000 remote=80
 const user = require("./routes/user");
 const recipes = require("./routes/recipes");
 const auth = require("./routes/auth");
-
 
 //#region cookie middleware
 app.use(function (req, res, next) {
@@ -87,15 +84,15 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500).send({ message: err.message, success: false });
 });
 
+// const server = app.listen(port, () => {
+//   console.log(`Server listen on port ${port}`);
+// });
 
+// process.on("SIGINT", function () {
+//   if (server) {
+//     server.close(() => console.log("server closed"));
+//   }
+//   process.exit();
+// });
 
-const server = app.listen(port, () => {
-  console.log(`Server listen on port ${port}`);
-});
-
-process.on("SIGINT", function () {
-  if (server) {
-    server.close(() => console.log("server closed"));
-  }
-  process.exit();
-});
+module.exports = app;
